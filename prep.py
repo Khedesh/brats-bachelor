b800d04d571311a7b2b2029420b5814d785b59f0
سیدمحمد حسینی <smhosseini@khedesh.ir>
2018-08-27 20:11:11 +0430

    update

--- prep.py
+++ prep.py
@@ -22,19 +22,22 @@ def generate_data(start, end):
             print('Data', i, 'preprocess...')
             data_i = data_i.astype(np.float64)
             # z-score normalization
-            data_i -= np.mean(data_i)
-            data_i /= np.std(data_i)
-            data_i = np.pad(data_i, (16,), 'constant', constant_values=(0,))
+            # data_i -= np.mean(data_i)
+            # data_i /= np.std(data_i)
+            data_i = np.pad(data_i, ((0,), (16,), (16,)), 'constant', constant_values=(0,))
             # label_i = label_i.reshape((155, 240, 240, 1))
-            for x in range(16, 256):
-                for y in range(16, 256):
-                    print('Patch: ', x, ',', y, 'on', i)
-                    if i < 20:
-                        data = np.append(data, data_i[x - 16:x + 16, y - 16:y + 16])
-                        label = np.append(label, label_i[x - 16, y - 16])
-                    else:
-                        data_test = np.append(data_test, data_i[x - 16:x + 16, y - 16:y + 16])
-                        label_test = np.append(label_test, label_i[x, y])
+            for q in range(0, data_i.shape[0]):
+                for x in range(16, 256):
+                    for y in range(16, 256):
+                        if data_i[q][x][y] == 0:
+                            continue
+                        print('Patch:', q, ',', x, ',', y, 'on', i)
+                        if i < 200:
+                            data = np.append(data, data_i[q][x - 16:x + 16, y - 16:y + 16])
+                            label = np.append(label, label_i[q][x - 16, y - 16])
+                        else:
+                            data_test = np.append(data_test, data_i[x - 16:x + 16, y - 16:y + 16])
+                            label_test = np.append(label_test, label_i[x, y])
 
             print(data.shape, label.shape, data_test.shape, label_test.shape)
             f = gzip.GzipFile('process/data.' + start + '.npy.gz', 'w')
@@ -56,7 +59,8 @@ if __name__ == '__main__':
 
     page = 20
     start = 0
-    while start < len(brd):
-        print('Start:', start)
-        executor.submit(generate_data, start, start + page)
-        start += page
+    generate_data(0, 2)
+    # while start < len(brd):
+    #     print('Start:', start)
+    #     executor.submit(generate_data, start, start + page)
+    #     start += page
